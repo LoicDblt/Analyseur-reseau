8ebca495ec4cc7b75be4aa71e6f6702df6abe4c6
Loïc DIEBOLT <loic@dblt.fr>
2022-11-12 16:50:18 +0100

    Corrections pour Linux

--- inc/ipv6.h
+++ inc/ipv6.h
@@ -7,6 +7,19 @@
 
 #include "ipv4.h"
 
+// Source : <netinet/ip6.h> MacOS (Ventura)
+#if BYTE_ORDER == BIG_ENDIAN
+	#define IPV6_FLOWINFO_MASK	0x0fffffff /* flow info (28 bits) */
+	#define IPV6_FLOWLABEL_MASK	0x000fffff /* flow label (20 bits) */
+	#define IPV6_FLOW_ECN_MASK	0x00300000 /* the 2 ECN bits */
+#else
+#if BYTE_ORDER == LITTLE_ENDIAN
+	#define IPV6_FLOWINFO_MASK	0xffffff0f /* flow info (28 bits) */
+	#define IPV6_FLOWLABEL_MASK	0xffff0f00 /* flow label (20 bits) */
+	#define IPV6_FLOW_ECN_MASK	0x00003000 /* the 2 ECN bits */
+#endif /* LITTLE_ENDIAN */
+#endif
+
 /************* FONCTIONS **************/
 
 // Fonction de gestion du protocole IPv6
--- src/ipv6.c
+++ src/ipv6.c
@@ -10,11 +10,10 @@ void gestionIPv6(const u_char* paquet, const int offset){
 
 	if (niveauVerbo > CONCIS){
 		printf("Flow info : 0x%06x\n",
-			ntohl(ip6->ip6_ctlun.ip6_un1.ip6_un1_flow & IPV6_FLOWINFO_MASK));
+			ntohl(ip6->ip6_flow & IPV6_FLOWINFO_MASK));
 
 		printf("Explicit congestion notification : ");
-		unsigned int ecn = ntohl(ip6->ip6_ctlun.ip6_un1.ip6_un1_flow &
-			IPV6_FLOW_ECN_MASK);
+		unsigned int ecn = ntohl(ip6->ip6_flow & IPV6_FLOW_ECN_MASK);
 		if (ecn == 0)
 			printf("Not ECN-capable transport");
 		else
@@ -22,9 +21,10 @@ void gestionIPv6(const u_char* paquet, const int offset){
 		printf(" (%d)\n", ecn);
 
 		printf("Flow label : 0x%06x\n",
-			ntohl(ip6->ip6_ctlun.ip6_un1.ip6_un1_flow & IPV6_FLOWLABEL_MASK));
-		printf("Payload length : %d\n", ntohs(ip6->ip6_ctlun.ip6_un1.ip6_un1_plen));
-		printf("Hop limit : %d\n", ip6->ip6_ctlun.ip6_un1.ip6_un1_hlim);
+			ntohl(ip6->ip6_flow & IPV6_FLOWLABEL_MASK));
+
+		printf("Payload length : %d\n", ntohs(ip6->ip6_plen));
+		printf("Hop limit : %d\n", ip6->ip6_hlim);
 	}
 
 	inet_ntop(AF_INET6, &ip6->ip6_src, buffAddrIPv6, INET6_ADDRSTRLEN);
@@ -34,7 +34,7 @@ void gestionIPv6(const u_char* paquet, const int offset){
 
 	if (niveauVerbo > CONCIS)
 		printf("\nNext header : ");
-	unsigned int proto = ip6->ip6_ctlun.ip6_un1.ip6_un1_nxt;
+	unsigned int proto = ip6->ip6_nxt;
 	switch (proto){
 		/* TCP */
 		case TCP:
@@ -42,7 +42,7 @@ void gestionIPv6(const u_char* paquet, const int offset){
 				printf("TCP (%d)", proto);
 
 			gestionTCP(paquet, offset + sizeof(struct ip6_hdr),
-				ntohs(ip6->ip6_ctlun.ip6_un1.ip6_un1_plen));
+				ntohs(ip6->ip6_plen));
 			break;
 
 		/* UDP */
